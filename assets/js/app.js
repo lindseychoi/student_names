68e28f3b59ff3392e84257338194f859ceae514f
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-01-17 19:21:51 -0700

    added to the js and did the pseudo coding

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+/** 
+ * take in the name 
+ * have a click event listener that adds the names into list (submit) --event.preventDefault
+ * check as long as it has a length
+ * put the names in an array
+ * store the array in local storage
+ * randomize the list of names in local storage
+ * 
+ */
+
+// variables
+const studentName = document.getElementById("student");
+const students = []; 
+const formEl = document.getElementById("submit");
+// functions
+function readForm(event) {
+    event.preventDefault();
+    console.log("readForm");
+    
+}
+// special functions
+formEl.addEventListener("click", readForm);
+// logic 
